84e7ee17d373be893c14ed5ce7c06675d5b5285f
Jared Schaab <jared.schaab@gmail.com>
2019-08-05 13:35:25 -0700

    commit bad stuff

---
+++ courses/02-static-analysis/src/example.js
@@ -0,0 +1,10 @@
+const firstName = 'Freddy';
+typeof firstName === 'string';
+
+if (!('serviceWorker' in navigator)) {
+  // you have an old browser :-(
+}
+
+const greeting = 'hello';
+console.log(`${greeting} world!`);
+[(1, 2, 3)].forEach(x => console.log(x));
